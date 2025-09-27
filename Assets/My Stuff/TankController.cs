7f47307e152ee8ca5a766d8b2512f352f7f0f1fa
Wally <tuong.hostudent@hcmut.edu.vn>
2025-09-27 23:18:46 +0700

    WIP stuff
    Camera work, minimap, game implementation placeholder

---
+++ Assets/My Stuff/TankController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // New Input System
+
+public class TankController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;      // conventional: movement speed
+    public float rotationSpeed = 180f; // conventional: degrees per second
+
+    private Vector2 moveInput;        // user-defined: stores WASD/Stick input
+    private Rigidbody2D rb;           // conventional: tank body
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Called from Input System "Move" action
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Forward/back movement (Y axis of input)
+        float moveAmount = moveInput.y * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + (Vector2)transform.up * moveAmount);
+
+        // Rotation (X axis of input)
+        float rotationAmount = -moveInput.x * rotationSpeed * Time.fixedDeltaTime;
+        rb.MoveRotation(rb.rotation + rotationAmount);
+    }
+}
